30cdc658dade69d8f9bf6dcdedb0db79f0836517
Aditya Kumar <adityaara7667@gmail.com>
2026-06-14 17:19:00 +0530

    add more

--- e_commerce/flipkart/urls.py
+++ e_commerce/flipkart/urls.py
@@ -4,6 +4,17 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('product/', views.product, name='product'),
+    path('category/', views.category, name='category'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('cart/', views.cart, name='cart'),
+    path('my-profile/', views.my_profile, name='my_profile'),
+    path('my-orders/', views.my_orders, name='my_orders'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+    path('register/verify-otp/', views.verify_register_otp, name='verify_register_otp'),
     path('login/', views.login_view, name='login'),
 ]
