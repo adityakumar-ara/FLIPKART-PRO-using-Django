ccd14b8962cfe31e0b59e01b7bd0175917b44a4a
Aditya Kumar <adityaara7667@gmail.com>
2026-07-05 10:43:11 +0530

    add more

--- e_commerce/flipkart/tests.py
+++ e_commerce/flipkart/tests.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Product
+from .models import Cart, Product
 
 
 class HomePageTests(TestCase):
@@ -26,3 +27,29 @@ class HomePageTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(owner_selected, response.context['top_products'])
         self.assertEqual(len(response.context['top_products']), 1)
+
+    def test_buy_now_uses_selected_quantity(self):
+        user = get_user_model().objects.create_user(
+            username='buyer',
+            email='buyer@example.com',
+            password='testpassword123',
+            full_name='Buyer',
+            mobile_no='9999999999',
+        )
+        product = Product.objects.create(
+            name='Quantity Product',
+            price=150.00,
+            image='product_images/qty.png',
+            stock=10,
+            is_active=True,
+        )
+
+        self.client.force_login(user)
+        response = self.client.post(
+            reverse('buy_now', args=[product.id]),
+            {'quantity': 3},
+        )
+
+        self.assertRedirects(response, reverse('checkout'))
+        cart_item = Cart.objects.get(user=user, product=product)
+        self.assertEqual(cart_item.quantity, 3)
